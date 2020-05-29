0364c6a64c195020359e31c9c8f302b5e17ac3d5
Priyanka7011 <pk3201134@gmail.com>
2020-05-29 07:35:04 +0530

    Updates

---
+++ precache-manifest.5d6be6837de6722cc4bda3e802188aca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b7df1eea6c2b02fd980fd894c07854",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "158a29f4e30d7d0ab083",
+    "url": "/calculator/static/css/main.0a51cf85.chunk.css"
+  },
+  {
+    "revision": "db923a253c586dc8f945",
+    "url": "/calculator/static/js/2.6205449a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.6205449a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "158a29f4e30d7d0ab083",
+    "url": "/calculator/static/js/main.ef8d0f84.chunk.js"
+  },
+  {
+    "revision": "53122c127a7fe401835e",
+    "url": "/calculator/static/js/runtime-main.6de0c573.js"
+  }
+]);
